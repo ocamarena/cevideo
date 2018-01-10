66339c568acc7c6c90a457925aa5bbc7c7fd075c
ocamarena <imillan@hotelescity.com>
2018-01-10 15:28:44 -0600

    comandos faltantes en sql

--- sql.sql
+++ sql.sql
@@ -182,3 +182,10 @@ ALTER TABLE `hoteles`
 
 ALTER TABLE `hoteles`
   MODIFY `id` int(5) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=135;
+
+
+--Para arreglar el cagadero:
+use aplicat8_cevideo;
+alter table `hoteles` change `ip-code` `ipcode` int(5);
+ALTER TABLE `hoteles` ADD `version` INT(5) NOT NULL AFTER `ipcode`;
+UPDATE hoteles SET version = 0; 
